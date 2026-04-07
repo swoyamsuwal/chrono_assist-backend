7580ad0e24c8bbcb8e1293ede331580c369e5700
swoyam <swoyam@gmail.com>
2026-04-07 11:39:01 +0545

    update

--- backend/calendar_app/rbac_perms.py
+++ backend/calendar_app/rbac_perms.py
@@ -2,8 +2,13 @@ from rbac.permissions import HasRBACPermission
 
 class CanConnectGoogleCalendar(HasRBACPermission):
     feature = "calendar"
-    action = "execute"
+    action = "view"
 
 class CanSendCalendarPrompt(HasRBACPermission):
     feature = "calendar"
     action = "execute"
+    
+class CanViewCalendarPrompt(HasRBACPermission):
+    feature = "calendar"
+    action = "view"
+

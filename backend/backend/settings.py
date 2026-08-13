8438e4afa7882b3d762a97369dbffd1d09d16208
swoyam <swoyam@gmail.com>
2026-08-13 17:03:15 +0545

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,6 +121,7 @@ MIDDLEWARE = [
 # Tells DRF to accept Bearer <token> headers for authentication
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
